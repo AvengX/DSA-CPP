5bc40e3b16abaa3a04f112ea043b5780427aa361
AvengX <ayushraj7654321@gmail.com>
2026-01-27 10:41:03 +0530

    Implemented union of arrays

--- 04_vector/union.cpp
+++ 04_vector/union.cpp
@@ -5,12 +5,12 @@ int main(){
     int arr[]={1,2,3,4,5};
     int sizea=5;
     int brr[]={9,8,7,6};
-    int sizeb=5;
+    int sizeb=4;
 vector<int>ans;
 for(int i=0;i<sizea;i++){
     ans.push_back(arr[i]);
 }    
-for(int i=0;i<sizea;i++){
+for(int i=0;i<sizeb;i++){
     ans.push_back(brr[i]);
 }
 cout<<"Union of two arrays is:"<<endl;
